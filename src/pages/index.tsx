4ee442ea4493b85fc61778f37474282077bb6ebd
Geekles <tondjilee@gmail.com>
2022-10-07 20:55:35 +0100

    meta and manifest added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,11 @@ const Home: NextPage = () => {
             <Head>
                 <title>BOZOO</title>
                 <meta name="description" content="Afro NFT's marketplace"/>
+                <link rel="apple-touch-icon" href="/favicon.svg"/>
+                <meta name="copyright" content="TONDJI NIAT J. Lee - Web & Mobile developer"/>
+                <meta name="author" content="TONDJI NIAT J. Lee"/>
+                <meta name="title" content="Afro NFTs Marketplace"/>
+                <link rel="manifest" href="/manifest.json"/>
                 <link rel="icon" href="/favicon.svg"/>
             </Head>
 
